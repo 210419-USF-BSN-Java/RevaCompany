0b0e543fef1d56021dcc6811f3ec449e21067abc
uri-oma <omamalinuriah@gmail.com>
2021-04-28 17:13:56 -0500

    finished department+employee implementation

--- src/main/java/com/revature/controllers/FrontController.java
+++ src/main/java/com/revature/controllers/FrontController.java
@@ -1,21 +1,46 @@
 package com.revature.controllers;
 
+import java.util.List;
+
 import com.revature.daos.DepartmentPostgres;
+import com.revature.daos.EmployeePostgres;
 import com.revature.models.Department;
-import com.revature.services.DepartmentService;
+import com.revature.models.Employee;
 
 public class FrontController {
 
-	private static DepartmentService ds;
+	// private static DepartmentService ds;
 	
 	public static void main(String[] args) {
 		
 		DepartmentPostgres dp = new DepartmentPostgres();
-		System.out.println(dp.getAll());
-		Department d = new Department();
-		d.setName("Sales");
-		d.setMonthlyBudget(7000.00);
-		System.out.println(dp.add(d));
-		System.out.println(dp.getAll());
+		EmployeePostgres ep = new EmployeePostgres();
+		
+//		System.out.println(dp.getAll());
+//		Department d = new Department();
+//		d.setName("Sales");
+//		d.setMonthlyBudget(7000.00);
+//		System.out.println(dp.add(d));
+//		System.out.println(dp.getAll());
+		System.out.println(dp.getById(5));
+		System.out.println(dp.update(new Department(2, "Test", 4000.55)));
+		System.out.println(dp.add(new Department(8, "Another Test", 3030.03)));
+		System.out.println(dp.delete(new Department(8, "Another Test", 3030.03)));
+		
+		System.out.println(ep.add(new Employee(0, "TEST EMPLOYEE", "TESTER", 20.02f, dp.getById(2), ep.getById(4))) + "\n");
+		
+		List<Employee> li = ep.getAll();
+		for (Employee e : li) {
+			System.out.println(e);
+		}
+		
+		Employee test = new Employee(14, "NEW TEST", "HELLO", 40.04f, dp.getById(6), ep.getById(8));
+		System.out.println(ep.update(test));
+		System.out.println(ep.delete(new Employee(16, "", "", 0f, new Department(), new Employee())) + "\n");
+		
+		li = ep.getByName("TEST EMPLOYEE");
+		for (Employee e : li) {
+			System.out.println(e);
+		}
 	}
 }
