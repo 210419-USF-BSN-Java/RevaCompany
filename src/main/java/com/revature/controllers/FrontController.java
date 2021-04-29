f596b21a39df9cf866a9b926203c775dc2abe5b5
espinalc1 <39393149+espinalc1@users.noreply.github.com>
2021-04-28 23:25:08 -0400

    Revert "project complete"

--- src/main/java/com/revature/controllers/FrontController.java
+++ src/main/java/com/revature/controllers/FrontController.java
@@ -1,17 +1,15 @@
 package com.revature.controllers;
 
 import com.revature.daos.DepartmentPostgres;
-import com.revature.daos.EmployeePostgres;
 import com.revature.models.Department;
-import com.revature.models.Employee;
 import com.revature.services.DepartmentService;
 
 public class FrontController {
 
 	private static DepartmentService ds;
-
+	
 	public static void main(String[] args) {
-
+		
 		DepartmentPostgres dp = new DepartmentPostgres();
 		System.out.println(dp.getAll());
 		Department d = new Department();
@@ -19,31 +17,5 @@ public class FrontController {
 		d.setMonthlyBudget(7000.00);
 		System.out.println(dp.add(d));
 		System.out.println(dp.getAll());
-		d.setMonthlyBudget(8000.00);
-		System.out.println(dp.update(d));
-		System.out.println(dp.delete(d));
-		System.out.println(dp.getById(2));
-
-		EmployeePostgres ep = new EmployeePostgres();
-		ep.getAll().stream().forEach(em -> System.out.println(em));
-
-		Department dept = dp.getById(2);
-		Employee manager = new Employee();
-		manager.setId(2);
-
-		Employee emp = new Employee();
-		emp.setName("FirstName LastName");
-		emp.setMonthlySalary(2000.00f);
-		emp.setPosition("Manager");
-		emp.setManager(manager);
-		emp.setDepartment(dept);
-
-		System.out.println(ep.add(emp));
-		emp.setMonthlySalary(3000.00f);
-
-		System.out.println(ep.update(emp));
-		System.out.println(ep.delete(emp));
-		System.out.println(ep.getById(85)); // shows that the deletion was correct!
-
 	}
 }
--- src/main/java/com/revature/daos/DepartmentPostgres.java
+++ src/main/java/com/revature/daos/DepartmentPostgres.java
@@ -12,41 +12,42 @@ import com.revature.models.Department;
 
 import util.ConnectionUtil;
 
-public class DepartmentPostgres implements DepartmentDao {
-
-	public DepartmentPostgres() {
-	}
+public class DepartmentPostgres implements DepartmentDao{
 
 	@Override
 	public Department add(Department t) {
 		/*
-		 * Department is beingpassed in Take in the fields add a record to the
-		 * department table Insert into tableName (col1, col2) values(x,y); insert into
-		 * departments (dept_name, monthly budget) values (t.getName(), t.getBudget());
-		 * get connection create prepared statement from connection setting the
-		 * variables execute
-		 * 
+		 * Department is being passed in
+		 * Take in the fields
+		 * add a record to the department table
+		 * Insert into tableName (col1, col2) values(x,y);
+		 * insert into departments (dept_name, monthly budget) values (t.getName(), t.getBudget());
+		 * get connection
+		 * create prepared statement from connection
+		 * setting the variables
+		 * execute
+
 		 */
 		Department department = null;
-		String sql = "insert into revcomp_schema.departments (dept_name, monthly_budget) values (?,?) returning dept_id;";
+		String sql = "insert into departments (dept_name, monthly_budget) values (?,?) returning dept_id;";
 //		String sql = "insert into departments (dept_name, monthly_budget) values (?,?);";
-		String[] keys = { "dept_id" };
-
-		try (Connection con = ConnectionUtil.getConnectionFromEnv()) {
+		String[] keys = {"dept_id"};
+		
+		try(Connection con = ConnectionUtil.getConnectionFromEnv()){
 //			PreparedStatement ps = con.prepareStatement(sql,keys);
 			PreparedStatement ps = con.prepareStatement(sql);
-			ps.setString(1, t.getName());
+			ps.setString(1,t.getName());
 			ps.setDouble(2, t.getMonthlyBudget());
-
+			
 			ResultSet rs = ps.executeQuery();
 //			ps.executeUpdate();
 //			ResultSet rs = ps.getGeneratedKeys();
-
-			if (rs.next()) {
+			
+			if(rs.next()) {
 				department = t;
 				department.setId(rs.getInt(1));
 			}
-
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -57,39 +58,20 @@ public class DepartmentPostgres implements DepartmentDao {
 	@Override
 	public Department getById(Integer id) {
 		// TODO Auto-generated method stub
-		Department department = null;
-		String sql = "select * from revcomp_schema.departments where dept_id = ?;";
-
-		try {
-			Connection c = ConnectionUtil.getConnectionFromEnv();
-			PreparedStatement s = c.prepareStatement(sql);
-			s.setInt(1, id);
-			ResultSet rs = s.executeQuery();
-
-			if (rs.next()) {
-				int deptId = rs.getInt("dept_id");
-				String deptName = rs.getString("dept_name");
-				double budget = rs.getDouble("monthly_budget");
-				department = new Department(deptId, deptName, budget);
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return department;
+		return null;
 	}
 
 	@Override
 	public List<Department> getAll() {
 		List<Department> departments = new ArrayList<>();
-		String sql = "select * from revcomp_schema.departments;";
+		String sql = "select * from departments;";
 
 		try {
 			Connection c = ConnectionUtil.getConnectionFromEnv();
 			Statement s = c.createStatement();
 			ResultSet rs = s.executeQuery(sql);
-
-			while (rs.next()) {
+			
+			while(rs.next()) {
 				int deptId = rs.getInt("dept_id");
 				String deptName = rs.getString("dept_name");
 				double budget = rs.getDouble("monthly_budget");
@@ -105,62 +87,33 @@ public class DepartmentPostgres implements DepartmentDao {
 	@Override
 	public Integer update(Department t) {
 		// TODO Auto-generated method stub
-		String sql = "UPDATE revcomp_schema.departments SET dept_name = ?, monthly_budget = ? WHERE dept_id = ?";
-
-		int affectedrows = 0;
-
-		try (Connection c = ConnectionUtil.getConnectionFromEnv(); PreparedStatement pstmt = c.prepareStatement(sql)) {
-
-			pstmt.setString(1, t.getName());
-			pstmt.setDouble(2, t.getMonthlyBudget());
-			pstmt.setInt(3, t.getId());
-
-			affectedrows = pstmt.executeUpdate();
-
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-		return affectedrows;
+		return null;
 	}
 
 	@Override
 	public Integer delete(Department t) {
-		String SQL = "DELETE FROM revcomp_schema.departments WHERE dept_id = ?";
-
-		int affectedrows = 0;
-
-		try (Connection conn = ConnectionUtil.getConnectionFromEnv();
-				PreparedStatement pstmt = conn.prepareStatement(SQL)) {
-
-			pstmt.setInt(1, t.getId());
-
-			affectedrows = pstmt.executeUpdate();
-
-		} catch (SQLException ex) {
-			System.out.println(ex.getMessage());
-		}
-		return affectedrows;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public List<Department> getDepartmentsByMonthlyBudget(Double budget) {
-		String sql = "SELECT * FROM revcomp_schema.departments where monthly_budget = ?";
+		String sql = "SELECT * FROM departments where monthly_budget = ?";
 		List<Department> departments = new ArrayList<>();
-
+		
 		try {
 			PreparedStatement ps = ConnectionUtil.getConnectionFromEnv().prepareStatement(sql);
 			ps.setDouble(1, budget);
 			ResultSet rs = ps.executeQuery();
-
-			while (rs.next()) {
-				departments.add(new Department(rs.getInt("dept_id"), rs.getString("dept_name"),
-						rs.getDouble("monthly_budget")));
+			
+			while(rs.next()) {
+				departments.add(new Department(rs.getInt("dept_id"), rs.getString("dept_name"), rs.getDouble("monthly_budget")));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 		return departments;
 	}
 
