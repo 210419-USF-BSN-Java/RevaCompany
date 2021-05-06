06b2a0f088d6eedd41286cdcde35439bde07484b
tom <thomcdixon@gmail.com>
2021-05-06 12:01:26 -0700

    add html files and start retrieve depts from service in department servlet doGet

--- src/main/java/com/revature/controllers/DepartmentServlet.java
+++ src/main/java/com/revature/controllers/DepartmentServlet.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -10,39 +11,51 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.revature.daos.EmployeeDao;
-import com.revature.daos.EmployeePostgres;
+import com.revature.models.Department;
+import com.revature.services.DepartmentService;
+import com.revature.services.DepartmentServiceImplementation;
+
+// import com.revature.services.DepartmentService;
+
+// import com.revature.services.DepartmentServiceImplementation;
 
 import util.h2Util;
 
-public class DepartmentServlet extends HttpServlet{
-    
-    private h2Util h2= new h2Util();
+public class DepartmentServlet extends HttpServlet {
+
+    private h2Util h2 = new h2Util();
+    DepartmentService ds = new DepartmentServiceImplementation();
 
     public void init(ServletConfig config) {
-		try {
+        try {
             h2.setup();
         } catch (FileNotFoundException | SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-	}
+    }
 
-	
-	public void destroy() {
-		try {
+    public void destroy() {
+        try {
             h2.teardown();
         } catch (FileNotFoundException | SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-	}
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-        // TODO: Retrieve all departments
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        // TODO: Retrieve all departments
+        List<Department> depts = ds.getDepartments();
+
+        for (Department d : depts) {
+            System.out.println(d.toString());
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
         // TODO: Create a department
     }
 }
